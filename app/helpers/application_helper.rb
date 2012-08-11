6a16c7f3b758ff8643b55351f652ff436348a9f0
solucionario <solucionario42@gmail.com>
2012-08-11 17:27:30 -0300

    Helper de titulo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	# Retornar el titulo
+	def full_title(page_title)
+		base_title= "Solucionario"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
+
 end
