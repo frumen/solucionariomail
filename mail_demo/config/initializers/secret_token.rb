05389a9a6bbe70fdeb237c40a10f2167e1e02e56
Hernan Guetta Minali <hguetta@hotmail.com>
2012-10-26 15:55:41 -0300

    Ult Subida

---
+++ mail_demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MailDemo::Application.config.secret_token = 'be7503259d05329da4474bb78c3798913f0f0e5f1ea028ee4fc2ea6d8e50e855af7a63115b3bb4c6063121b93328df62135bf8dc5ecc1c4b30b0394a73665ed3'
---
+++ railsinstaller_demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = '9ba5a781c2662e7afeafa865970e55e3071747902370c1f37199927e22f4c58bc429a1b37f7baab0c72226fa3ee08a51e4fbecdc26af9212bd7e0973ed61f72f'
